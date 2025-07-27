7a7300cf1c5e5cd87ee4b007d385b06aa0a0a541
JAEIL1999 <pkyj040410@gmail.com>
2025-07-27 16:01:21 +0900

    STEP-01 피드백 2차 수정

--- src/main/java/gift/controller/AuthController.java
+++ src/main/java/gift/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping
 public class AuthController {
     private final KakaoService kakaoService;
 
