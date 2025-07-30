ed3b1e09fb3debab37b494b6112b0e01441bbd9d
JAEIL1999 <pkyj040410@gmail.com>
2025-07-30 17:36:42 +0900

    STEP-02 피드백 반영(역할분리)

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -5,9 +5,7 @@ import gift.dto.order.OrderResponseDto;
 import gift.model.Options;
 import gift.repository.OptionsRepository;
 import gift.repository.WishlistRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.NoSuchElementException;
 
@@ -20,10 +18,8 @@ public class OrderService {
         this.optionsRepository = optionsRepository;
         this.wishlistRepository = wishlistRepository;
     }
-    @Value("${kakao.KAPI_TEMPLATE_MESSAGE}")
-    private String KAPI_TEMPLATE_MESSAGE;
 
-    public OrderResponseDto Order(OrderRequestDto orderRequestDto) {
+    public OrderResponseDto order(OrderRequestDto orderRequestDto) {
         Options options = optionsRepository.findById(orderRequestDto.getOptionId())
                 .orElseThrow(() -> new NoSuchElementException("해당 옵션이 존재하지 않습니다."));
 
@@ -35,27 +31,5 @@ public class OrderService {
         return new OrderResponseDto(orderRequestDto.getOptionId(),
                 orderRequestDto.getQuantity(),
                 orderRequestDto.getMessage());
-    }
-
-    public String SendKakaoMessage(String access_token) {
-        String templateJson = """
-        {
-            "object_type": "text",
-            "text": "주문이 완료되었습니다. 배송이 시작되면 다시 알려드릴게요!",
-            "link": {
-                "web_url": "https://developers.kakao.com",
-                "mobile_web_url": "https://developers.kakao.com"
-            },
-            "button_title": "내 주문 확인하기"
-        }
-        """;
-        WebClient webClient = WebClient.create(KAPI_TEMPLATE_MESSAGE);
-        return webClient.post()
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .header("Authorization", "Bearer " + access_token)
-                .bodyValue("template_object="+templateJson)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
     }
 }
